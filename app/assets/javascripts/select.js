48fad847d5eb072b8b45a01501f334f0f9e1d167
YuraHodan <yura.hodan@gmail.com>
2018-06-05 19:04:22 +0300

    day1

--- app/assets/javascripts/select.js
+++ app/assets/javascripts/select.js
@@ -1,1 +1,8 @@
-
+function myFunction(){
+  $(".nav-button").click(function() {
+    /*  Toggle the CSS closed class which reduces the height of the UL thus
+        hiding all LI apart from the first */
+    $(this).parent().parent().toggleClass("closed");
+  });
+}
+myFunction()
--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,13 +1,4 @@
-//
-// $(".slider").slick({
-//
-//   infinite: true,
-//   dots: true,
-//   arrows: true,
-//   autoplay: false,
-//   nextArrow: $('.slider-container').find('.next-btn'),
-//   prevArrow: $('.slider-container').find('.prev-btn')
-// });
+
 $('.slider-for').slick({
  slidesToShow: 1,
  slidesToScroll: 1,
@@ -29,25 +20,55 @@ $('.slider-nav').slick({
 
 });
 
-// if ( width < 640) {
-//   $('.slider-nav').slick({
-//    slidesToShow: 1,
-//    slidesToScroll: 1,
-//    asNavFor: '.slider-for',
-//    dots: false,
-//    centerMode: false,
-//    focusOnSelect: false,
-//    autoplay: false,
-//    autoplaySpeed: 1000,
-//    nextArrow: $('.mainlidet').find('.next-btn'),
-//    prevArrow: $('.mainlidet').find('.prev-btn')
-// }
-
-
 $('.slider-nav-small').slick({
  autoplay: true,
  autoplaySpeed: 2000,
  dots: false,
  prevArrow: false,
  nextArrow: false
+});
+
+
+/// goood slider 1
+
+$('.goode1-slider-for').slick({
+ slidesToShow: 1,
+ slidesToScroll: 1,
+ arrows: false,
+ fade: true,
+ asNavFor: '.goode1-slider-nav'
+});
+$('.goode1-slider-nav').slick({
+ slidesToShow: 1,
+ slidesToScroll: 1,
+ asNavFor: '.goode1-slider-for',
+ focusOnSelect: true,
+ autoplay: true,
+ autoplaySpeed: 2000,
+ nextArrow: $('.mainlidet').find('.next-btn'),
+ prevArrow: $('.mainlidet').find('.prev-btn')
+
+});
+
+
+
+/// goood slider 2
+
+
+$('.goode2-slider-for').slick({
+ slidesToShow: 1,
+ slidesToScroll: 1,
+ arrows: false,
+ fade: true,
+ asNavFor: '.goode2-slider-nav'
+});
+$('.goode2-slider-nav').slick({
+ slidesToShow: 3,
+ slidesToScroll: 3,
+ asNavFor: '.goode2-slider-for',
+ autoplay: true,
+ autoplaySpeed: 2000,
+ nextArrow: $('.mainlidet').find('.next-btn'),
+ prevArrow: $('.mainlidet').find('.prev-btn')
+
 });
