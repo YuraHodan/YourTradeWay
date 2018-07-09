17711eae12c51da7db5ed8bc5ee20eba28a152fa
YuraHodan <yura.hodan@gmail.com>
2018-07-09 17:19:36 +0300

    smol css

--- app/assets/javascripts/angular/contacts/contactsCtrl.js
+++ app/assets/javascripts/angular/contacts/contactsCtrl.js
@@ -6,7 +6,6 @@ $scope.phoneNumbr = /^\+?\d{2}[- ]?\d{3}[- ]?\d{5}$/;
 		$scope.submitForm = function() {
 			// check to make sure the form is completely valid
 			if ($scope.userForm.$valid) {
-        // alert('lox');
         // $scope.user = {};
         // $scope.userForm.$setPristine();
 			}
