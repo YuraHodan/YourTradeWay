c7aaae52cce5640728638d5af160be0a1545c526
YuraHodan <yura.hodan@gmail.com>
2018-09-12 17:53:26 +0300

    menu bag jq end angularjs mainjs 29

--- app/assets/javascripts/angular/menus/menusCtrl.js
+++ app/assets/javascripts/angular/menus/menusCtrl.js
@@ -22,27 +22,68 @@
 // }])
 
 
-ytwApp.controller('menusControler',function($scope){
+// ytwApp.controller('menusControler',function($scope,$http ){
+//
+//
+//   // ytwApp.factory("itemsService", ['$http', function($q, $http){
+//   //
+//   //     //   Return all items from ITEMS collection
+//   //     return {
+//   //
+//   //         ytwAppmenu: function () {
+//   //             $http({method: 'GET', url: '/api/v1/menus.json'}).
+//   //             then(function success(response){
+//   //                 deferred.resolve(response.data);
+//   //                 console.log('zoro');
+//   //             }, function error(response){
+//   //                 deferred.reject(response.status);
+//   //                 console.log('zoro2');
+//   //             });
+//   //             return deferred.promise;
+//   //         }
+//   //     }
+//   // }])
+//   $http({method: 'GET', url: '/api/v1/menus.json'}).
+//       then(function success(response) {
+//           $scope.question=response.data.question;
+//   });
+//
+//   $scope.voteUp = function (answer){
+//       answer.rate++;
+//   };
+//   $scope.voteDown = function (answer){
+//       answer.rate--;
+//   };
+// });
 
+// ytwApp.controller('QuestionController',
+//     function QuestionController($scope, $http){
+//
+//         $http({method: 'GET', url: '/api/v1/menus.json'}).
+//             then(function success(response) {
+//                 $scope.question=response.data.question;
+//         });
+//
+//         $scope.voteUp = function (answer){
+//             answer.rate++;
+//         };
+//         $scope.voteDown = function (answer){
+//             answer.rate--;
+//         };
+//     }
+// )
+ytwApp.controller('QuestionController',
+    function QuestionController($scope, $http){
 
-  ytwApp.factory("itemsService", [ '$q', '$http', function($q, $http){
+        $http({method: 'GET', url: '/api/v1/menus.json'}).
+            then(function success(response) {
+                $scope.question=response.data.products;
+                $scope.question2=response.data.markets;
+        });
 
-      //   Return all items from ITEMS collection
-      return {
-
-          ytwAppmenu: function () {
-              var deferred = $q.defer();
-              $http({method: 'GET', url: '/api/v1/menus.json'}).
-              then(function success(response){
-                  deferred.resolve(response.data);
-                  console.log('zoro');
-              }, function error(response){
-                  deferred.reject(response.status);
-                  console.log('zoro2');
-              });
-              return deferred.promise;
-          }
-      }
-  }])
-});
-console.log('666');
+        $scope.voteUp = function (answer){
+        };
+        $scope.voteDown = function (answer){
+        };
+    }
+)
