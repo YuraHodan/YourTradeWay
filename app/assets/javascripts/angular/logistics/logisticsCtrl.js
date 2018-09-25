16b41c4c05c8e182ab003d94a2ac7faf98c5cd18
YuraHodan <yura.hodan@gmail.com>
2018-09-25 18:02:37 +0300

    create filters without http

--- app/assets/javascripts/angular/logistics/logisticsCtrl.js
+++ app/assets/javascripts/angular/logistics/logisticsCtrl.js
@@ -21,6 +21,45 @@ ytwApp.controller('logisticsController',
                 $scope.top_logistics=response.data.top_logistics;
                 $scope.all_logistics=response.data.logistics;
                 $scope.recom_logistics=response.data.recomend_logistics;
+                $scope.all_countries=response.data.countries;
+                $scope.current_page=response.data.current_page;
+                $scope.pages_count=response.data.pages_count;
         });
+        $scope.list = [];
+        $scope.submit = function() {
+          if ($scope.text) {
+            $scope.list.push(this.text);
+            console.log($scope.list);
+          }
+        };
+              // console.log($scope.list);
+        // $scope.submitForm = function () {
+        //     var url = 'logistics';
+        //     var data = {
+        //         info: $scope.all_countries
+        //     };
+        //     var config = {
+        //         headers : {
+        //             'Content-Type': 'application/json',
+        //             'X-CSRF-Token': token
+        //         }
+        //     };
+        //
+        //     $http.post(url, data, config)
+        //         .then(
+        //             function(response){
+        //                 // callback
+        //                 $scope.all_countries = data.all_countries;
+        //                 alert("gooooo.");
+        //             },
+        //             function(response){
+        //                 // failure callback
+        //                 alert("no");
+        //             }
+        //         );
+        // }
+
     }
+
+
 )
