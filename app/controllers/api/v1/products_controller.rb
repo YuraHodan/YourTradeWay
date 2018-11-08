73ac6e88584a407b5d3e7d102f7f55dd7491768c
Viktor <vodarchenko@gmail.com>
2018-11-08 16:47:45 +0200

    fix filter

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -22,6 +22,8 @@ class Api::V1::ProductsController < Api::V1::BaseController
   def filtered_products(products)
     filtered_data = products
     filtered_data = filtered_data.where(third_menu_id: filter_ids) if filter_ids.any?
+    filtered_data << filtered_data.where(second_menu_id: second_ids) if second_ids.any?
+    filtered_data = filtered_data&.uniq
     filtered_data = filtered_data.where(country_id: params[:country_ids]) if params[:country_ids].present?
     map_products(filtered_data.paginate(page: current_page, per_page: 12))
   end
@@ -31,6 +33,13 @@ class Api::V1::ProductsController < Api::V1::BaseController
     array << SecondMenu.where(main_menu_id: params[:type_ids]).map(&:third_menus).map(&:ids) if params[:type_ids].present?
     array << SecondMenu.where(id: params[:group_ids]).map(&:third_menus).map(&:ids) if params[:group_ids].present?
     array << ThirdMenu.where(id: params[:category_ids]).pluck(:id) if params[:category_ids].present?
+    array = array.flatten.uniq
+  end
+
+  def second_ids
+    array = []
+    array << SecondMenu.where(main_menu_id: params[:type_ids]).map(&:third_menus).map(&:ids) if params[:type_ids].present?
+    array << SecondMenu.where(id: params[:group_ids]).map(&:third_menus).map(&:ids) if params[:group_ids].present?
     array = array.flatten.uniq
   end
 
